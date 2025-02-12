5e35a6e49f7278ac61bacadbabb27d7021417956
LydianJay <lloydjayedradan@gmail.com>
2025-02-12 16:45:52 +0800

    added db electricity data

---
+++ app/Models/ElectricityData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ElectricityData extends Model
+{
+    //
+    protected $table = 'electricity_data';
+    protected $primaryKey = 'id';
+    
+}

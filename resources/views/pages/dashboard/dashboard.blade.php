9a9c7d735836d522945cc7512d453f9d81cafe01
LydianJay <lloydjayedradan@gmail.com>
2025-02-12 15:25:04 +0800

    some suftt

---
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -0,0 +1,24 @@
+<x-basedashboard>
+    <section>
+        <div class="page-header min-vh-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+                        <div class="card card-plain">
+                            <div class="card-header pb-0 text-start">
+                                <h4 class="font-weight-bolder">Sign In</h4>
+                                <p class="mb-0">Enter your email and password to sign in</p>
+                            </div>
+                            <div class="card-body">
+                                <form role="form">
+                                    <div class="mb-3">
+                                        <input type="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="email" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
+                                    </div>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="rememberMe">    
+                                    </div>
+
+</x-basedashboard>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.login');
 });
+
+
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard.dashboard');
+});
+
+
